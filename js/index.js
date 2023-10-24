979034547f633500c5cbf7b9f39eff81c787be64
Jan Maaß <git@antipost.de>
2023-10-24 16:19:04 +0200

    Add solution

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('[data-js="age-input"]');
 const button = document.querySelector('[data-js="age-button"]');
-const output = document.querySelector('[data-js="age-output"]');
+let output = document.querySelector('[data-js="age-output"]');
 
 button.addEventListener("click", () => {
   // Number(x) converts the string from the input to a number
@@ -10,4 +10,9 @@ button.addEventListener("click", () => {
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the output.
+  if (age >= 14 && age <= 19) {
+    output.textContent = "You are a teen.";
+  } else {
+    output.textContent = "You are not a teen.";
+  }
 });
